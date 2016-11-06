b189dc4e5f0bc30d1da57423ad442c0ffbb00a32
Alexander Rykalin <Alexander.Rykalin@returnonintelligence.com>
2016-11-06 04:57:56 +0300

    CTT-70672 elastice search cleanup and structured

--- search/search.py
+++ search/search.py
@@ -8,129 +8,34 @@ from config import PORTAL_LIST
 from config import OPENAM_LIST
 
 class e_search(object):
-    def __init__(self, host):
+    def __init__(self, host, source, match, size=1):
         self.log = logging.getLogger("errbot.plugins.%s" % self.__class__.__name__)
         self.host = host
+        self.source = source
+        self.match = match
+        self.size = size
     def exec(self):
         self.log.debug('host inventory in search {}'.format(self.host))
-        es = Elasticsearch(['http://dev-test-elk.carpathia.com:9200'])
-        our_q = {"from":0,"size":1,"query":{"bool":{"should":[{"match":{"host":self.host}}, {"match":{' \
-                '"source":"*catalina.out"}}, {"match":{"message":"ERROR*"}}]}}}
+        es = Elasticsearch(ELASTIC_SERVER)
+        our_q = {"from":0,"size":self.size,"query":{"bool":{"should":[{"match":{"host":self.host}}, {"match":{' \
+                '"source":self.source}}, {"match":{"message":self.match+"*"}}]}}}
         res = es.search(index="logstash-*",body=our_q)
         print("Got %d Hits:" % res['hits']['total'])
         for hit in res['hits']['hits']:
             print("%(@timestamp)s %(host)s: %(message)s" % hit["_source"])
 
-# host = "app01"
-# hostname = look_for_host_in_host_list.search(host,PORTAL_LIST)
-# print(hostname)
 class Search(BotPlugin):
     """
     Make various searches
     """
-    def activate(self):
-        """
-        Triggers on plugin activation
 
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Search, self).activate()
+    @arg_botcmd('host', type=str)
+    @arg_botcmd('--size', dest='size', type=int, default=1)
+    @arg_botcmd('--match', dest='match', type=str, default="ERROR")
+    @arg_botcmd('--source', dest='source', type=str, default="catalina.out")
+    def latest_erro(self, mess, host=None, size=None, match=None, source=None):
+        s = e_search(host,source,match,size)
+        yield s.exec()
 
-    def deactivate(self):
-        """
-        Triggers on plugin deactivation
 
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Search, self).deactivate()
 
-    def get_configuration_template(self):
-        """
-        Defines the configuration structure this plugin supports
-
-        You should delete it if your plugin doesn't use any configuration like this
-        """
-        return {'EXAMPLE_KEY_1': "Example value",
-                'EXAMPLE_KEY_2': ["Example", "Value"]
-               }
-
-    def check_configuration(self, configuration):
-        """
-        Triggers when the configuration is checked, shortly before activation
-
-        Raise a errbot.utils.ValidationException in case of an error
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        super(Search, self).check_configuration(configuration)
-
-    def callback_connect(self):
-        """
-        Triggers when bot is connected
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_message(self, message):
-        """
-        Triggered for every received message that isn't coming from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    def callback_botmessage(self, message):
-        """
-        Triggered for every message that comes from the bot itself
-
-        You should delete it if you're not using it to override any default behaviour
-        """
-        pass
-
-    @webhook
-    def example_webhook(self, incoming_request):
-        """A webhook which simply returns 'Example'"""
-        return "Example"
-
-    # Passing split_args_with=None will cause arguments to be split on any kind
-    # of whitespace, just like Python's split() does
-    @botcmd(split_args_with=None)
-    def example(self, message, args):
-        """A command which simply returns 'Example'"""
-        return "Example"
-
-    @arg_botcmd('name', type=str)
-    @arg_botcmd('--favorite-number', type=int, unpack_args=False)
-    def hello(self, message, args):
-        """
-        A command which says hello to someone.
-
-        If you include --favorite-number, it will also tell you their
-        favorite number.
-        """
-        if args.favorite_number is None:
-            return "Hello {name}".format(name=args.name)
-        else:
-            return "Hello {name}, I hear your favorite number is {number}".format(
-                name=args.name,
-                number=args.favorite_number,
-            )
-
-    @botcmd
-    def latest_erro(self, msg, args):
-        es = Elasticsearch(ELASTIC_SERVER)
-
-        #es_query = '{"query":{"bool":{"should":[{"match":{"host":"dev-test-app05*"}},{"match":{"source":"*catalina.out"}}]}}}'
-        #es_query = '{"query":{"bool":{"should":[{"match":{"host":"dev-test-app05*"}},{"match":{"source":"*catalina.out"}},
-        # {"match":{"message":"ERROR*"}}]}}}'
-        es_query = '{"from":0,"size":5,"query":{"bool":{"should":[{"match":{"host":"dev-test-app05*"}},{"match":{' \
-                '"source":"*catalina.out"}},{"match":{"message":"ERROR*"}}]}}}'
-
-        #res = es.search(index="logstash-2016.10.31",body=es_query)
-        res = es.search(index="logstash-*",body=es_query)
-
-        yield ("Got %d Hits:" % res['hits']['total'])
-
-        for hit in res['hits']['hits']:
-            yield ("%(@timestamp)s %(host)s: %(message)s" % hit["_source"])
